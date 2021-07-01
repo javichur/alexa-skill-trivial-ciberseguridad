9e86ca8b24221b5065154be2da317d2f835c0e35
javichur <javichur@gmail.com>
2021-07-01 17:57:40 +0200

    Update questions js.

--- lambda/custom/data/preguntas-trivial.js
+++ lambda/custom/data/preguntas-trivial.js
@@ -448,6 +448,186 @@ module.exports = {
       aclaracion: 'para detectar un perfil falso, podemos analizar el número de seguidores que tiene en relación con los perfiles que sigue, o comprobar si su imagen de perfil o el resto de imágenes publicadas no sean suyas, mediante Google imágenes.',
     },
     {
+      id: 22,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 23,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 24,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 25,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 26,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 27,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 28,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 29,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 30,
+      categoria: 'rrss',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
       id: 31,
       categoria: 'dispositivos',
       enunciado: 'de las siguientes opciones, ¿cuáles son medidas antimalware con las que proteger nuestro dispositivo?',
@@ -466,6 +646,186 @@ module.exports = {
         },
       ],
       aclaracion: 'dentro de las medidas de protección, el antivirus y el cortafuegos (firewall) se encuentran entre las mas básicas. Su función es la de protegernos ante distintos tipos de ataque a la seguridad de nuestro equipo.',
+    },
+    {
+      id: 32,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 33,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 34,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 35,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 36,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 37,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 38,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 39,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 40,
+      categoria: 'dispositivos',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
     },
     {
       id: 41,
@@ -488,6 +848,186 @@ module.exports = {
       aclaracion: 'al igual que las aplicaciones, estos complementos pueden ser maliciosos o presentar vulnerabilidades. Lo mejor es descargarlos de sitios oficiales y revisar los comentarios y valoraciones de otros usuarios.',
     },
     {
+      id: 42,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 43,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 44,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 45,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 46,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 47,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 48,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 49,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
+      id: 50,
+      categoria: 'navegacion',
+      enunciado: '',
+      respuestas: [
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false,
+        },
+        {
+          respuesta: '',
+          correcta: false, 
+        },
+      ],
+      aclaracion: '',
+    },
+    {
       id: 51,
       categoria: 'fraudes',
       enunciado: 'uno de tus contactos te ha compartido una noticia sobre una cura para una enfermedad muy contagiosa. ¿Cómo comprobarías si se trata de una noticia real?',
@@ -508,26 +1048,186 @@ module.exports = {
       aclaracion: 'internet esta lleno de bulos y noticias falsas. Para combatir la desinformacion, es recomendable contrastar la informacion con otras fuentes.',
     },
     {
-      id: xxxx,
-      categoria: '',
-      enunciado: '',
+      id: 52,
+      categoria: 'fraudes',
+      enunciado: 'te ha llegado un SMS sobre una ayuda económica de 350 euros. Solo hay que acceder a un link y rellenar un formulario. ¿Qué haces?',
       respuestas: [
         {
-          respuesta: '',
+          respuesta: 'investigo sobre la ayuda en Internet',
+          correcta: true,
+        },
+        {
+          respuesta: 'accedo, ya que por SMS no hay riesgo',
           correcta: false,
         },
         {
-          respuesta: '',
-          correcta: false,
-        },
-        {
-          respuesta: '',
+          respuesta: 'copio el enlace y accedo desde modo incognito',
           correcta: false, 
         },
       ],
-      aclaracion: '',
+      aclaracion: 'los ciberdelincuentes pueden utilizar estos formularios para hacerse con nuestra información más personal. Para evitario, recuerda contrastar e investigar la informacion del mensaje con otras fuentes y pasar el cursor por el link para asegurarte que te redirige a un sitio legítimo.',
+    },
+    {
+      id: 53,
+      categoria: 'fraudes',
+      enunciado: 'has recibido un correo, donde un desconocido afirma tener material íntimo sobre ti. Te pide realizar un pago a cambio de no difundirlo. ¿Qué deberias hacer?',
+      respuestas: [
+        {
+          respuesta: 'ignorar el mensaje',
+          correcta: true,
+        },
+        {
+          respuesta: '¿Y si es verdad? Pagas para evitar problemas',
+          correcta: false,
+        },
+        {
+          respuesta: 'desconfias. Le pides una prueba primero',
+          correcta: false, 
+        },
+      ],
+      aclaracion: 'este tipo de ataque se conoce como sextorsión. Tratan de asustarnos diciendo que tienen material comprometido sobre nosotros para que realicemos un pago a cambio de supuestamente no difundirlo.',
+    },
+    {
+      id: 54,
+      categoria: 'fraudes',
+      enunciado: 'ante un fraude donde acaban suplantando tu identidad, debes ejercer tus derechos ARCO. Qué significan sus siglas? Qué deberías hacer?',
+      respuestas: [
+        {
+          respuesta: 'Acceso, Rectificación, Comprobación y Oposición',
+          correcta: false,
+        },
+        {
+          respuesta: 'Acceso, Rectificación, Cancelación y Oposición.',
+          correcta: true,
+        },
+        {
+          respuesta: 'Afirmación, Rectificación, Comprobación y Oposición',
+          correcta: false, 
+        },
+      ],
+      aclaracion: 'los derechos ARCO hacen referencia al derecho de Acceso, Rectificación, Cancelación y Oposición que tenemos en relación al uso que se hace de nuestros datos.',
+    },
+    {
+      id: 55,
+      categoria: 'fraudes',
+      enunciado: 'recibes un SMS de tu compañía de teléfono informándote de un error en tu última factura. Te piden que hagas clic en un enlace, pero al entrar y descargar el archivo, se trataba de un malware. ¿Cómo se conoce a este fraude?',
+      respuestas: [
+        {
+          respuesta: 'grooming',
+          correcta: false,
+        },
+        {
+          respuesta: 'baitering',
+          correcta: false,
+        },
+        {
+          respuesta: 'smishing',
+          correcta: true, 
+        },
+      ],
+      aclaracion: 'este tipo de ciberataque se conoce como smishing, utilizan los SMS para engañarnos y hacer que nos descarguemos malware o que hagamos clic en webs maliciosas.',
     },
 
+    {
+      id: 56,
+      categoria: 'fraudes',
+      enunciado: '¿qué factores te deberían hacer desconfiar al tratar con un vendedor de una web de compraventa online?',
+      respuestas: [
+        {
+          respuesta: 'quiere utilizar la plataforma de pago de la web',
+          correcta: false,
+        },
+        {
+          respuesta: 'quiere seguir la comunicación por WhatsApp.',
+          correcta: true,
+        },
+        {
+          respuesta: 'tiene varios meses de antigüedad en la web con muchas valoraciones',
+          correcta: false, 
+        },
+      ],
+      aclaracion: 'si un vendedor trata de utilizar otro canal de comunicación o forma de pago distinto al de la web, podría tratarse de un fraude, es una práctica muy habitual utilizada entre los ciberdelincuentes.',
+    },
+    {
+      id: 57,
+      categoria: 'fraudes',
+      enunciado: 'acaba de llegarte una oferta de trabajo muy interesante. Sin embargo, algo te huele mal. ¿Cuál de las siguientes opciones sería un indicio de fraude?',
+      respuestas: [
+        {
+          respuesta: 'pide contactar con un numero de tarificacion especial',
+          correcta: false,
+        },
+        {
+          respuesta: 'pide dinero como gastos de administración por adelantado',
+          correcta: false,
+        },
+        {
+          respuesta: 'ambas opciones',
+          correcta: true, 
+        },
+      ],
+      aclaracion: 'aquellas ofertas de empleo que piden llamar a un teléfono de tarificación especial o solicitan un dinero por adelantado son candidatas a tratarse de un fraude.',
+    },
+    {
+      id: 58,
+      categoria: 'fraudes',
+      enunciado: 'has recibido una llamada de un supuesto soporte técnico. Tras seguir los pasos indicados, te das cuenta de que ha podido tratarse de un fraude. ¿Qué deberías hacer?',
+      respuestas: [
+        {
+          respuesta: 'cambiar las contraseñas de todas tus cuentas',
+          correcta: false,
+        },
+        {
+          respuesta: 'desinstalar las apps que hayan instalado en tu equipo',
+          correcta: false,
+        },
+        {
+          respuesta: 'todas las opciones son correctas',
+          correcta: true, 
+        },
+      ],
+      aclaracion: 'Si creemos ser víctima de un posible fraude de este tipo, lo más importante es deshabilitar cualquier cambio en nuestra configuración, desinstalar cualquier app y cambiar las contraseñas de nuestros equipos. Un análisis con nuestro antivirus también sería recomendable.',
+    },
+    {
+      id: 59,
+      categoria: 'fraudes',
+      enunciado: 'es hora de cambiarse de casa y estás mirando alquileres por tu zona. ¿Cuál de las siguientes opciones no te ayudará a evitar fraudes?',
+      respuestas: [
+        {
+          respuesta: 'comprobar las direcciones en Google Maps',
+          correcta: false,
+        },
+        {
+          respuesta: 'revisar las descripciones e imagenes en busca de fallos o dobles',
+          correcta: false,
+        },
+        {
+          respuesta: 'desconfiar de las agencias',
+          correcta: true, 
+        },
+      ],
+      aclaracion: 'la clave para detectar posibles fraudes en los alquileres es revisar detenidamente las descripciones e imágenes. Además, conviene utilizar Google Maps y Google Imágenes para comprobar que la dirección y las fotos utilizadas son reales.',
+    },
+    {
+      id: 60,
+      categoria: 'fraudes',
+      enunciado: 'Acabas de recibir un correo de tu marketplace favorita, donde te avisan de una promoción por la cual te regalan 50 euros. Solo tienes que acceder al enlace para obtener el código. ¿Qué debes hacer?',
+      respuestas: [
+        {
+          respuesta: 'acceder al enlace y reclamar el código',
+          correcta: false,
+        },
+        {
+          respuesta: 'pasar el cursor sobre el enlace para ver si dirige a la web original',
+          correcta: true,
+        },
+        {
+          respuesta: 'copiar el enlace y acceder desde modo incognito',
+          correcta: false, 
+        },
+      ],
+      aclaracion: 'para evitar acabar siendo víctima de un fraude, recuerda pasar el cursor por encima para ver la web real a la que te redirige.',
+    }
   ],
 };
 
